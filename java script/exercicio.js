00f5523369309e773fd3f4671cf67afa9f478f95
jujutrald <129910359+jujutrald@users.noreply.github.com>
2023-06-21 21:50:53 -0300

    tutorial de como colar na prova de matematica kkkk
    bem.joia.joia

---
+++ java script/exercicio.js
@@ -0,0 +1,63 @@
+/*
+let pagamento = "debito";
+
+//if(pagamento ==="debito"){
+    //console.log("receba 10% de desconto");
+//}else if(pagamento === dinheiro || pagamento === "pix"){
+    //console.log("15% de desconto")
+//}else if(pagamento === "2x"){
+    //console.log("preço normal de etiqueta sem juros")
+//}else{
+    //console.log("preço normal de etiqueta sem juros de 10%")
+//}
+
+
+switch (pagamento){
+    case "debito":
+    console.log("receba 10% de desconto");
+    break;
+    case "dinheiro":
+    console.log("receba 15% de desconto");
+    break;
+    case "pix":
+    console.log("receba 15% de desconto");
+    break;
+    case "2x":
+    console.log("preço normal de etiqueta sem juros");
+    break;
+    default:
+    console.log("preço normal de etiqueta sem juros se 10%");
+    break;
+}
+*/
+//let carro = "renegade";
+
+//switch (carro){
+    //case "renegade":
+    //console.log("jeep");
+    //break;
+    //case "c4 cactus":
+    //console.log("citroen");
+    //break;
+    //case "hb20":
+    //console.log("hyundai");
+    //break;
+    //case "kicks":
+    //console.log("nissan");
+    //break;
+    // "focus":
+    //console.log("ford");
+    //break;
+    //case "tracker":
+    //console.log("chevrolet");
+    //break;
+    //case "corola":
+    //console.log("toyota");
+    //break;
+    //default:
+   //console.log("escolha entre renegade, c4 cactus, hb20, kicks, focus, tracker, corola");
+    //break;
+//}
+for (let i = 0; i < 10; i++){
+    console.log("o resultado de 5 x é", i, "=", 5 * i);
+}
