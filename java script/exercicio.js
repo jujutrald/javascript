a21fe7f632b5551e4b00e1bbcedfbb8720451f3a
jujutrald <129910359+jujutrald@users.noreply.github.com>
2023-06-26 21:53:45 -0300

    asndfcweqhrgiuWKLARH
    legal e bunito.joia

--- java script/exercicio.js
+++ java script/exercicio.js
@@ -58,6 +58,64 @@ switch (pagamento){
    //console.log("escolha entre renegade, c4 cactus, hb20, kicks, focus, tracker, corola");
     //break;
 //}
-for (let i = 0; i < 10; i++){
-    console.log("o resultado de 5 x é", i, "=", 5 * i);
+//for (let i = 0; i < 10; i++){
+    //console.log("o resultado de 5 x é", i, "=", 5 * i);
+//}
+
+//let i =0;
+//while (i <=10) {
+    //console.log("O resultado de 5 x", i, "=", 5 * i);
+    //i++
+//}
+//let num1 = 5;
+//let num2 = 10;
+
+ //let soma = num1+num2;
+
+//console.log(soma);
+//function soma(x, y) {
+//const resultado = x + y;
+//return resultado;
+//}
+//console.log(soma(20, 30));
+//console.log(soma(15, 55));
+
+//function saudacao(){
+    //return "Boa noite pessoal";
+//}
+//console.log(saudacao());
+
+//function saud(nome3){
+    //return `Boa noite ${nome3}`;
+//}
+//console.log(saud("Maria"));
+
+let altura2 = 1.80;
+let peso2 = 80;
+let imc2 = peso2 / (altura2 * altura2)
+function imc(peso, altura){
+    let resultado = peso / (altura * altura)
+    if (resultado <= 24.9) {
+     return "Peso normal"   
+} else if(resultado>=25 && resultado <=29.9){
+    return "pessoa com sobrepeso"
+} else if(resultado>=29.9)
+
+if (resultado <= 30) {
+    return "Obesidade grau I"   
+} else if(resultado>=30 && resultado <=34.9){
+   return "pessoa com obesidade grau I"
+} else if(resultado>=34.9) 
+
+if (resultado <= 35) {
+    return "Obesidade grau II"  
+ } else if(resultado>=35 && resultado <=39.9){
+   return "pessoa com obesidade grau II"
+} else if(resultado>=39.9)
+
+if (resultado <= 40) {
+    return "Obesidade grau III"   
+} else if(resultado>=40 && resultado <=45.9){
+   return "pessoa com obesidade grau III"
+} else if(resultado>=45.9)
 }
